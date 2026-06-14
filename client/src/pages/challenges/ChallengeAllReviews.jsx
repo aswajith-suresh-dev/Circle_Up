501f838299501bee1f5d9982ecc630d24a93b263
Aswajith Suresh <aswajithsuresh2006@gmail.com>
2026-06-14 20:52:38 +0530

    challengeall  review file change

--- client/src/pages/challenges/ChallengeAllReviews.jsx
+++ client/src/pages/challenges/ChallengeAllReviews.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api/axios";
-import "../../css/ChallengeReviews.css";
+import "../../css/challengeReviews.css";
 
 const ChallengeAllReviews = () => {
 
