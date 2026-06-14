771fb7f6d0e69e02b614b26f3a41e2b8755236f5
Aswajith Suresh <aswajithsuresh2006@gmail.com>
2026-06-14 20:57:21 +0530

    folder name change

--- client/src/pages/personal/PersonalSpace.jsx
+++ client/src/pages/personal/PersonalSpace.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../api/axios";
-import "../../css/personal/personalSpace.css";
+import "../../css/personal/PersonalSpace.css";
 
 import {
   FiEdit2,
